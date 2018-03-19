a3f1bf8454bf8da1a0b0624a513350701f3f3636
teoboley <teo751@gmail.com>
2018-03-19 12:49:48 -0400

    Deleted alias example.

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
 
-// Example of using paths alias via tsconfig.json to easily re-use legacy code
-// Also, use TypeScript and JavaScript interchangeably.
-import aliasExample from '@myPathAlias/component'
-
 export default () => (
   <div>
     <h1>This is what we're all about.</h1>
@@ -11,6 +7,5 @@ export default () => (
       React, static sites, performance, speed. It's the stuff that makes us
       tick.
     </p>
-    {aliasExample}
   </div>
 )
