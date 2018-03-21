58c64645bcf6aaf12189650c4e97a9be2a93b352
Dustin <dustinknopoff@gmail.com>
2018-03-20 22:45:11 -0400

    about me?

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 export default () => (
   <div>
-    <h1>This is what we're all about.</h1>
+    <h1>About Us</h1>
+    <blockquote>
+      We find animal testing to be an abhorrent practice. So we didn't.
+    </blockquote>
     <p>
-      React, static sites, performance, speed. It's the stuff that makes us
-      tick.
+      L'Oreal is dedicated to the world standard in excellence of beauty
+      products and ethical testing. Give Hair Today is an initiative to promote
+      the strength of women through beauty.
     </p>
+    <img
+      src="https://res.cloudinary.com/dknopoff/image/upload/f_auto/v1521600205/lorealethics.png"
+      alt="L'Oreal Ethics"
+    />
   </div>
 )
